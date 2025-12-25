3d91736ee402de11c9a78a147684a6115e20c7cb
amirshah07 <144581429+amirshah07@users.noreply.github.com>
2025-12-25 14:08:42 +0800

    updated about me section

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -3,7 +3,7 @@ import type { PortfolioData } from '../types';
 export const portfolioData: PortfolioData = {
   name: "Amir Shah",
   
-  aboutMe: "Hi, I'm Amir! I'm a student at the National University of Singapore (NUS), double majoring in Data Science & Analytics and Computer Science. I enjoy building all kinds of projects that are functional, user-friendly, and thoughtfully designed. I'm always keen to learn new technologies and turn ideas into useful, working products.",
+  aboutMe: "Hi, I’m Amir! I’m a student at the National University of Singapore (NUS), double majoring in Data Science & Analytics and Computer Science. I enjoy building projects and bringing ideas to life through code. My current interests lie in data engineering.",
 
   skills: [
     /*
